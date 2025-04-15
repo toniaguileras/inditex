8a20b6ad55731b6e2a4ee7036c5ae02410281966
toniaguileras <toniaguileras@gmail.com>
2025-04-15 18:51:59 +0200

    first commit

---
+++ src/main/java/toni/aguilera/inditex/InditexApplication.java
@@ -0,0 +1,13 @@
+package toni.aguilera.inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexApplication.class, args);
+	}
+
+}
